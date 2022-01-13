4a6d13ae5051ec4007416ca783c21b7b8bc55d3f
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2022-01-13 13:47:30 -0300

    Video 2.3

--- primeiro-projeto/cypress/integration/3-ALURAPIC/alurapic.spec.js
+++ primeiro-projeto/cypress/integration/3-ALURAPIC/alurapic.spec.js
@@ -15,5 +15,22 @@ describe('Login e registro de usuarios alura pic', () => {
         cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
     })
 
+    it('verifica mensagens de email invalido', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="email"]').type('jacqueline');
+        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
+     
+    })
+
+    it('verifica mensagens de senha com menos de 8 caracteres', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="password"]').type('123');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
+     
+    })
+
 })
 

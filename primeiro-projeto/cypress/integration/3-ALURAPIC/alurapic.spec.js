1c93a7a618886d317cfaa73cc7ef3625a1c80f7d
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2022-02-24 12:11:09 -0300

    Video 4.1

--- primeiro-projeto/cypress/integration/3-ALURAPIC/alurapic.spec.js
+++ primeiro-projeto/cypress/integration/3-ALURAPIC/alurapic.spec.js
@@ -44,5 +44,21 @@ describe('Login e registro de usuarios alura pic', () => {
         })
     })
 
+    
+    const usuarios = require('../../fixtures/usuarios.json');
+    usuarios.forEach(usuario => {
+
+        it(`registra novo usuario ${usuario.userName} `, () => {
+            cy.contains('a', 'Register now').click();
+            cy.contains('button', 'Register').click();
+            cy.get('input[formcontrolname="email"]').type(usuario.email);
+            cy.get('input[formcontrolname="fullName"]').type(usuario.fullName);
+            cy.get('input[formcontrolname="userName"]').type(usuario.userName);
+            cy.get('input[formcontrolname="password"]').type(usuario.password);
+            cy.contains('button', 'Register').click();
+        })
+    });
+
+
 })
 

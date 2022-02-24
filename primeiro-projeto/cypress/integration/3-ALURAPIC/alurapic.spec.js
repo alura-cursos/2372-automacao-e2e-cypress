5de7a3438a2c4197cace732d7ddb53f2d64c87f4
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2022-02-24 11:35:58 -0300

    video 3.1

--- primeiro-projeto/cypress/integration/3-ALURAPIC/alurapic.spec.js
+++ primeiro-projeto/cypress/integration/3-ALURAPIC/alurapic.spec.js
@@ -32,5 +32,17 @@ describe('Login e registro de usuarios alura pic', () => {
      
     })
 
+    it('fazer login de usuario valido', () => {
+        cy.login('flavio', '123');
+        cy.contains('a', '(Logout)').should('be.visible');
+    })
+
+    it('fazer login de usuario invalido', () => {
+        cy.login('jacqueline', '1234');
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('Invalid user name or password');
+        })
+    })
+
 })
 
